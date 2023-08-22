f5efa4ed3901320f211814b6f186044d9fbc5f79
MayurBadgujar123 <132994310+MayurBadgujar123@users.noreply.github.com>
2023-08-22 22:56:15 +0530

    Add files via upload

---
+++ Constructor/Employees.cs
@@ -0,0 +1,43 @@
+﻿namespace Constructor
+{
+    class Employees    //variables
+    {
+        int EmpId;
+        string EmpName;
+        int EmpAge;
+
+        public Employees(int EmpId, string EmpName, int EmpAge)  //parameterise constructor
+        {
+            this.EmpId = EmpId;
+            this.EmpName = EmpName;
+            this.EmpAge = EmpAge;
+
+        }
+
+        public int getId()
+        {
+            return this.EmpId;
+        }
+
+        public string getName()
+        {
+            return this.EmpName;
+        }
+        public int  getAge()
+        {
+            return this.EmpAge;
+
+        }
+        static void Main(string[] args)
+        {
+            Employees Mayur = new Employees(11,"Mayur B",26);
+            Employees Ali = new Employees(12, "Ali K", 22);
+
+
+            Console.WriteLine(Mayur.getId());
+            Console.WriteLine(Mayur.getName());
+            Console.WriteLine(Mayur.getAge());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorOverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConstructorOverloading
+{
+     class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("this is first constructor!!");
+        }
+
+        public Program(int a,int b)
+        {
+            Console.WriteLine("this is second constructor !! {0}", (a+b));
+
+        }
+        public Program(int a, int b ,int c)
+        {
+            Console.WriteLine("this is third constructor !! {0}", (a +b+c));
+
+        }
+        static void Main (String[] args)
+        {
+            Program p = new Program(20,10,5); //constructor overloading
+            Console.ReadLine();
+        }
+
+    }
+}
